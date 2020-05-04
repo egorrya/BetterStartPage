833d98f6ae0d29b9505792b541273887ddb86215
soldsite <fayncore@gmail.com>
2020-05-05 01:04:35 +0700

    deleted JQ Dependency and reafactor into ES6

--- newtab.js
+++ newtab.js
@@ -1,34 +1,41 @@
-$(function()
-{
-	chrome.bookmarks.getTree(function(itemTree)
-	{
-		var dom = { html: '' };
-		itemTree[0].children[0].children.forEach(function(child)
-		{
-			ProcessBookmarkNode(child, dom);
-		});
-		$('#bookmarks').html(dom.html);
-	});
+const ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+};
+
+ready(() => {
+  chrome.bookmarks.getTree((itemTree) => {
+    let dom = { html: "" };
+    itemTree[0].children[0].children.forEach((child) => {
+      ProcessBookmarkNode(child, dom);
+    });
+
+    document.getElementById("bookmarks").innerHTML = dom.html;
+  });
 });
 
-function ProcessBookmarkNode(node, dom)
-{
-	if (node.children)
-	{
-		dom.html += '<li><h2>' + node.title + '</h2><ul>';
-		node.children.forEach(function(child)
-		{
-			ProcessBookmarkNode(child, dom);
-		});
-		dom.html += '</ul></li>';
-	}
-	if (node.url)
-	{
-		dom.html += '<li><a href="' + node.url + '" title="' + node.url + '"><img src="chrome://favicon/' + node.url + '" />' + ShortenString(node.title, 30) + '</a></li>';
-	}
-}
+const ProcessBookmarkNode = (node, dom) => {
+  if (node.children) {
+    dom.html += "<li><h2>" + node.title + "</h2><ul>";
+    node.children.forEach((child) => {
+      ProcessBookmarkNode(child, dom);
+    });
+    dom.html += "</ul></li>";
+  }
+  if (node.url) {
+    dom.html +=
+      '<li><a href="' +
+      node.url +
+      '" title="' +
+      node.url +
+      '"><img src="chrome://favicon/' +
+      node.url +
+      '" />' +
+      ShortenString(node.title, 30) +
+      "</a></li>";
+  }
+};
 
-function ShortenString(str, length)
-{
-	return str.length > length ? str.substr(0, length - 3) + '...' : str;
-}
+const ShortenString = (str, length) => {
+  return str.length > length ? str.substr(0, length - 3) + "..." : str;
+};
