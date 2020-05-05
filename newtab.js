4ee66212c9f664b354a2fa8838d4aee9a54df1f3
soldsite <fayncore@gmail.com>
2020-05-05 19:51:58 +0700

    separate popup mockup

--- newtab.js
+++ newtab.js
@@ -44,3 +44,14 @@ const ProcessBookmarkNode = (node, dom) => {
 const ShortenString = (str, length) => {
   return str.length > length ? str.substr(0, length - 3) + "..." : str;
 };
+
+const addButtonTrigger = (el) => {
+  el.addEventListener("click", () => {
+    const popupEl = document.querySelector(`.${el.dataset.for}`);
+    popupEl.classList.toggle("popup--visible");
+  });
+};
+
+Array.from(document.querySelectorAll("button[data-for]")).forEach(
+  addButtonTrigger
+);
